6ad9b0182ab2f5ac66c37f2822cf1fee5c2b1f48
wwt-ztk <84946566+wwt-ztk@users.noreply.github.com>
2021-05-28 13:23:50 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,6 +14,7 @@ public class GitTest {
         System.out.println("hello,git4");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
 
     }
 }
