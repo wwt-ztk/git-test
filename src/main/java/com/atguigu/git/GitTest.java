d68b93fe7397300aa9c9ef6f874afb3ab4e14892
wwt <wwt@atguigu.com>
2021-05-28 11:55:59 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wwt
+ * @create 2021-05-28 上午11:51
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
